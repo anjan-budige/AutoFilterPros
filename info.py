e933ca46a826c2bd93a5d7befe8131ef51ec2999
Anjan Budige Altruist <61613373+anjan-budige@users.noreply.github.com>
2022-10-30 12:49:38 +0530

    Update info.py

--- info.py
+++ info.py
@@ -32,7 +32,7 @@ AUTH_CHANNEL = int(auth_channel) if auth_channel and id_pattern.search(auth_chan
 AUTH_GROUPS = [int(ch) for ch in auth_grp.split()] if auth_grp else None
 
 # MongoDB information
-DATABASE_URI = mongodb+srv://enrich:enrich@cluster0.mhghl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority
+DATABASE_URI = "mongodb+srv://enrich:enrich@cluster0.mhghl.mongodb.net/myFirstDatabase?retryWrites=true&w=majority"
 DATABASE_NAME = "M4uVideo"
 COLLECTION_NAME = 'Telegram_files'
 
