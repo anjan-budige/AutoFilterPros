61a66f81e7e818a47b09770d03123e5bf01cc93f
Anjan Budige Altruist <61613373+anjan-budige@users.noreply.github.com>
2022-12-07 07:17:35 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,12 +1,12 @@
 if [ -z $UPSTREAM_REPO ]
 then
   echo "Cloning main Repository"
-  git clone https://github.com/EvamariaTG/EvaMaria.git /EvaMaria
+  git clone https://github.com/anjan-budige/AutoFilterPros /AutoFilterPros
 else
   echo "Cloning Custom Repo from $UPSTREAM_REPO "
-  git clone $UPSTREAM_REPO /EvaMaria
+  git clone $UPSTREAM_REPO /AutoFilterPros
 fi
-cd /EvaMaria
+cd /AutoFilterPros
 pip3 install -U -r requirements.txt
 echo "Starting Bot...."
 python3 bot.py
